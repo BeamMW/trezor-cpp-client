109c185c118f6d3d50836e88526b8b750d354eaf
vldmkr <vladmakaryan@gmail.com>
2019-05-31 17:35:44 +0300

    small refactoring

---
+++ src/base_device_manager.hpp
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include "utils.hpp"
+#include "client.hpp"
+#include "queue/working_queue.h"
+#include "models/models.hpp"
+#include "debug.hpp"
+
+class BaseDeviceManager
+{
+public:
+  using MessageCallback = std::function<void(const Message &, std::string, size_t)>;
+  BaseDeviceManager();
+  virtual ~BaseDeviceManager() = 0;
+  void init(const Enumerate &enumerate);
+  void callback_Failure(MessageCallback callback);
+  void callback_Success(MessageCallback callback);
+
+protected:
+  const std::string GLOBAL_SESSION_ID = "global";
+
+  virtual void handle_custom_response(const Call &call, const std::string &session) = 0;
+  bool execute_callback(const Message &msg, int type, const std::string &session);
+  template <typename MessageType>
+  bool execute_callback(const Call &call, const std::string &session);
+  void call(std::string message, int type, MessageCallback callback) throw();
+
+private:
+  Client m_client;
+  WorkingQueue<Call, std::string> m_worker_queue;
+  WorkingQueue<bool, size_t> m_request_queue;
+
+  std::unordered_map<std::pair<int, std::string>, MessageCallback, pair_hash> m_callbacks;
+  std::string m_path = "null";
+  std::string m_session = "null";
+  bool m_is_real = false;
+  
+  void handle_response(const Call &call, const std::string &session);
+};
+
+BaseDeviceManager::BaseDeviceManager()
+{
+  m_worker_queue.setGlobalPopCallback(
+      [&](const std::string &session_pop, const Call &call) {
+        using namespace hw::trezor::messages;
+        if (MessageType_ButtonRequest == call.type)
+        {
+          m_worker_queue.push(session_pop, [&](const std::string &session) {
+            return m_client.call(session, pack_message(ButtonAck()));
+          });
+        }
+        else
+        {
+          auto released = m_client.release(session_pop);
+          if (released.session == m_session)
+            m_session = "null";
+
+          handle_response(call, released.session);
+
+          m_request_queue.unlockPop();
+        }
+      });
+}
+
+BaseDeviceManager::~BaseDeviceManager()
+{
+}
+
+void BaseDeviceManager::init(const Enumerate &enumerate)
+{
+  if (enumerate.session != "null")
+    throw std::runtime_error("device already occupied, complete the previous session first");
+
+  m_path = enumerate.path;
+  m_session = enumerate.session;
+  m_is_real = enumerate.vendor && enumerate.product;
+}
+
+void BaseDeviceManager::callback_Failure(MessageCallback callback)
+{
+  using namespace hw::trezor::messages;
+  auto key = std::make_pair(MessageType_Failure, GLOBAL_SESSION_ID);
+  m_callbacks[key] = callback;
+}
+
+void BaseDeviceManager::callback_Success(MessageCallback callback)
+{
+  using namespace hw::trezor::messages;
+  auto key = std::make_pair(MessageType_Success, GLOBAL_SESSION_ID);
+  m_callbacks[key] = callback;
+}
+
+bool BaseDeviceManager::execute_callback(const Message &msg, int type, const std::string &session)
+{
+  auto key = std::make_pair(type, session);
+  auto callback = m_callbacks.find(key);
+  if (callback != m_callbacks.end())
+  {
+    callback->second(msg, session, m_request_queue.size());
+    return true;
+  }
+  return false;
+}
+
+template <typename MessageType>
+bool BaseDeviceManager::execute_callback(const Call &call, const std::string &session)
+{
+  return execute_callback(call.to_message<MessageType>(), call.type, session);
+}
+
+void BaseDeviceManager::call(std::string message, int type, MessageCallback callback) throw()
+{
+  m_request_queue.push(m_request_queue.size(), [&, message, type, callback](size_t size) {
+    if (m_session != "null")
+    {
+      throw std::runtime_error("previous session must be completed");
+      // return false; //TODO: decide which better (throw or return false)
+    }
+
+    auto acquired = m_client.acquire(m_path, m_session);
+    if (acquired.error.empty())
+    {
+      m_session = acquired.session;
+      if (callback)
+      {
+        auto key = std::make_pair(type, m_session);
+        m_callbacks[key] = callback;
+      }
+      m_worker_queue.push(m_session, [&, message](const std::string &session) {
+        return m_client.call(session, message);
+      });
+
+      m_request_queue.lockPop();
+    }
+    return true;
+  });
+}
+
+void BaseDeviceManager::handle_response(const Call &call, const std::string &session)
+{
+  using namespace hw::trezor::messages;
+
+  switch (call.type)
+  {
+  case MessageType_Failure:
+    if (!execute_callback<Failure>(call, session))
+      execute_callback<Failure>(call, GLOBAL_SESSION_ID);
+    break;
+  case MessageType_Success:
+    if (!execute_callback<Success>(call, session))
+      execute_callback<Success>(call, GLOBAL_SESSION_ID);
+    break;
+  case INTERNAL_ERROR:
+  {
+    print_call_response(call);
+
+    m_session = "null";
+    m_request_queue.clear();
+    m_worker_queue.clear();
+
+    Failure error;
+    error.set_message(call.error);
+    execute_callback(error, MessageType_Failure, GLOBAL_SESSION_ID);
+    break;
+  }
+  default:
+    handle_custom_response(call, session);
+    break;
+  }
+}
--- src/device_manager.hpp
+++ src/device_manager.hpp
@@ -1,51 +1,12 @@
 #pragma once
 
-#include <string>
-#include <unordered_map>
-#include "client.hpp"
-#include "models/models.hpp"
-#include "queue/working_queue.h"
-#include "utils.hpp"
+#include "base_device_manager.hpp"
 
-#include "debug.hpp"
-
-class DeviceManager
+class DeviceManager: public BaseDeviceManager
 {
 public:
-  using MessageCallback = std::function<void(const Message &, std::string, size_t)>;
-
-  DeviceManager()
+  virtual ~DeviceManager()
   {
-    m_worker_queue.setGlobalPopCallback(
-        [&](const std::string &session_pop, const Call &call) {
-          using namespace hw::trezor::messages;
-          if (MessageType_ButtonRequest == call.type)
-          {
-            m_worker_queue.push(session_pop, [&](const std::string &session) {
-              return m_client.call(session, pack_message(ButtonAck()));
-            });
-          }
-          else
-          {
-            auto released = m_client.release(session_pop);
-            if (released.session == m_session)
-              m_session = "null";
-
-            handle_response(call, released.session);
-
-            m_request_queue.unlockPop();
-          }
-        });
-  }
-
-  void init(const Enumerate &enumerate)
-  {
-    if (enumerate.session != "null")
-      throw std::runtime_error("device already occupied, complete the previous session first");
-
-    m_path = enumerate.path;
-    m_session = enumerate.session;
-    m_is_real = enumerate.vendor && enumerate.product;
   }
 
   void call_Ping(std::string text, bool button_protection, MessageCallback callback)
@@ -106,113 +67,22 @@ public:
     message.release_kidv();
   }
 
-  void callback_Failure(MessageCallback callback)
-  {
-    using namespace hw::trezor::messages;
-    auto key = std::make_pair(MessageType_Failure, GLOBAL_SESSION_ID);
-    m_callbacks[key] = callback;
-  }
-
-  void callback_Success(MessageCallback callback)
-  {
-    using namespace hw::trezor::messages;
-    auto key = std::make_pair(MessageType_Success, GLOBAL_SESSION_ID);
-    m_callbacks[key] = callback;
-  }
-
 protected:
-  void handle_response(const Call &call, const std::string &session)
+  virtual void handle_custom_response(const Call &call, const std::string &session)
   {
     using namespace hw::trezor::messages;
     using namespace hw::trezor::messages::beam;
-    
+
     switch (call.type)
     {
-    case MessageType_Failure:
-      execute_callback<Failure>(call, GLOBAL_SESSION_ID);
-      break;
-    case MessageType_Success:
-      if (!execute_callback<Success>(call, session))
-        execute_callback<Success>(call, GLOBAL_SESSION_ID);
-      break;
     case MessageType_BeamOwnerKey:
       execute_callback<BeamOwnerKey>(call, session);
       break;
     case MessageType_BeamPublicKey:
       execute_callback<BeamPublicKey>(call, session);
       break;
-    case INTERNAL_ERROR:
-    {
-      print_call_response(call);
-
-      m_session = "null";
-      m_request_queue.clear();
-      m_worker_queue.clear();
-
-      Failure error;
-      error.set_message(call.error);
-      execute_callback(error, MessageType_Failure, GLOBAL_SESSION_ID);
-      break;
-    }
     default:
       break;
     }
   }
-
-  bool execute_callback(const Message &msg, int type, const std::string &session)
-  {
-    auto key = std::make_pair(type, session);
-    auto callback = m_callbacks.find(key);
-    if (callback != m_callbacks.end())
-    {
-      callback->second(msg, session, m_request_queue.size());
-      return true;
-    }
-    return false;
-  }
-
-  template <typename MessageType>
-  bool execute_callback(const Call &call, const std::string &session)
-  {
-    return execute_callback(call.to_message<MessageType>(), call.type, session);
-  }
-
-  void call(std::string message, int type, MessageCallback callback) throw()
-  {
-    m_request_queue.push(m_request_queue.size(), [&, message, type, callback](size_t size) {
-      if (m_session != "null")
-      {
-        throw std::runtime_error("previous session must be completed");
-        // return false; //TODO: decide which better (throw or return false)
-      }
-
-      auto acquired = m_client.acquire(m_path, m_session);
-      if (acquired.error.empty())
-      {
-        m_session = acquired.session;
-        if (callback) {
-          auto key = std::make_pair(type, m_session);
-          m_callbacks[key] = callback;
-        }
-        m_worker_queue.push(m_session, [&, message](const std::string &session) {
-          return m_client.call(session, message);
-        });
-
-        m_request_queue.lockPop();
-      }
-      return true;
-    });
-  }
-
-private:
-  const std::string GLOBAL_SESSION_ID = "global";
-
-  Client m_client;
-  WorkingQueue<Call, std::string> m_worker_queue;
-  WorkingQueue<bool, size_t> m_request_queue;
-
-  std::unordered_map<std::pair<int, std::string>, MessageCallback, pair_hash> m_callbacks;
-  std::string m_path = "null";
-  std::string m_session = "null";
-  bool m_is_real = false;
 };
